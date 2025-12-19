86b6b5519f6267c16c858a225192b7e79c7f32fa
Kim Long Ngo <ngokimlong2007@gmail.com>
2025-12-19 12:24:46 -0500

    Mocking Initialization

--- 02-Spring Boot Unit Test/src/test/java/com/kimlngo/component/ApplicationExampleTest.java
+++ 02-Spring Boot Unit Test/src/test/java/com/kimlngo/component/ApplicationExampleTest.java
@@ -10,7 +10,6 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.context.ApplicationContext;
-import org.springframework.test.context.event.annotation.BeforeTestMethod;
 
 import java.util.List;
 
